c48d0ebbc2e09816ab6071303ed4f378029df694
Andrei <39308844+Silfarion@users.noreply.github.com>
2018-05-24 20:48:42 +0300

    Add files via upload

---
+++ bloks/header.php
@@ -0,0 +1,13 @@
+<!-- начало хедера -->
+<div id="header">
+<!-- между метками HHH и DDD будет наполнение хедера -->
+<!-- HHH -->
+<table width="100%" height="100px" border="0" cellspacing="0">
+  <tr>
+    <td align="center" width="200px"><img src="img/logo.jpg" width="200px" height="97px" alt=""></td>
+    <td align="center" width="1000px"><img src="img/img_heder.jpg" width="1000px" height="97px" alt=""></td>
+  </tr>
+</table>
+<!-- DDD -->
+</div><!-- #header-->
+<!-- конец хедера -->
---
+++ bloks/sideLeft.php
@@ -0,0 +1,35 @@
+<!-- левый сайдбар -->
+ <div class="sidebar" id="sideLeft">
+<!-- наполнение левого сайдбара между метками CCC и DDD -->
+<!-- CCC -->
+<div class="menu">
+<strong>вставить красивое<br> меню для сайта</strong>
+    <ul>
+     <li><a href="index.php">главная</a></li>
+     <li><a href="page 1.php">описание динамики</a></li>
+     <li><a href="page 2.php">описание сайдбара</a></li>
+     <li><a href="temp.html">шаблон для порезки</a></li>
+     <li><a href="#">раздел 4</a></li>
+     <li><a href="#">раздел 5</a></li>
+     <li><a href="#">раздел 6</a></li>
+     <li><a href="#">раздел 7</a></li>
+    </ul>
+    <br><br>
+ </div>
+ <br> 
+ <br>
+ <div class="reklama">
+ <p class="p_1" >
+ Здесь<br> можно<br> расположить<br> баннеры
+ </p>
+ <p class="p_1" >
+ Здесь<br> можно<br> расположить<br> рекламные ссылки
+ </p>
+ <p class="p_1" >
+ Свои другие материалы.
+ </p>
+ </div>  
+<!-- DDD -->
+ </div>
+ <!-- .sidebar#sideLeft -->
+</div><!-- #middle-->
